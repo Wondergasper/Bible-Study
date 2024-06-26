f5be6a379e8c42a29e15e5218cd040dd703899be
gasper wonder <gasperwonder@gmail.com>
2024-06-26 15:17:39 +0100

    mark changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Logo from './components/Logo/Logo';
+import Logo from './Component/Logo/Logo';
 import Home from './pages/Home';
 import About from './pages/About/About';
-import Footer from './components/Footer/Footer';
+import Footer from './Component/Footer/Footer';
 import Stream from './pages/Stream/Stream';
 import Discuss from './pages/Discuss/Discuss';
-import Preloader from './Component/preloader/preloader'; // Make sure the path is correct
+//import Preloader from './Component/preloader/preloader'; // Make sure the path is correct
 import './App.css';
 
 const App = () => {
-  const [isLoading, setLoading] = useState(true);
+  // const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false); 
-    }, 2000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false); 
+  //   }, 2000);
+  // }, []);
 
   return (
     <Router>
-      {isLoading && <Preloader />}
+      {/* {isLoading && <Preloader />} */}
       <Logo />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/stream" element={<Stream />} />
         <Route path="/discuss" element={<Discuss />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
       </Routes>
       <Footer />
     </Router>
